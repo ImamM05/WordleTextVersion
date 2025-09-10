254879f54f314faa19dc6a8effae707932fc02c2
ImamM05 <imammishkat@gmail.com>
2025-09-09 20:44:00 -0400

    Initial commit

---
+++ WordleLogic.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class WordleLogic
+{
+    private final int AMOUNT_OF_TRIES = 5;
+    private final HashMap[] wordList = {
+    };
+    private HashMap<String, Integer> foundWords;
+    private int cursor;
+
+
+}
