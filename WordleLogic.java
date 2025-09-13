31cda1799d1488752e5f2d20f13cf112d387cab9
ImamM05 <imammishkat@gmail.com>
2025-09-13 15:45:29 -0400

    wordle game implementation

--- WordleLogic.java
+++ WordleLogic.java
@@ -5,18 +5,23 @@ import java.util.HashMap;
 public class WordleLogic
 {
     private final int MAX_TRIES_ALLOWED = 5;
-    private final String[] wordList = {
-        "APPLE",
-        "HELLO",
-        "WORLD",
-        "GLOAT",
-        "FIGHT",
-        "SIGHT",
-        "LIGHT",
-        "RIGHT",
-    };
+    private String[] wordList = new String[20];
     private int cursor;
     private HashMap<Character, Integer> lettFreqMap = new HashMap<>();
+    private String currWord;
+    private String modVal;
+    private boolean gameOver = false;
 
+    public WordleLogic(String[] arr) {
+        wordList = arr;
+        cursor = -1;
+    }
 
+    private void setWord() {
+        cursor++;
+    }
+
+    private boolean isWordAcceptable(String userInput) {
+        return true; // placeholder
+    }
 }
