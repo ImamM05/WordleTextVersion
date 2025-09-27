59630d6c2d98d2b8621da599bde1568384035422
ImamM05 <imammishkat@gmail.com>
2025-09-27 13:27:50 -0400

    1. updated WordleLogic.txt with new logic, and added more code

--- Runner.java
+++ Runner.java
@@ -2,7 +2,13 @@ public class Runner
 {
     public static void main(String [] args)
     {
-        
+        String[] words = {"HELLO", "WORLD"};
+        String input = "WORLD";
+
+        WordleLogic game1 = new WordleLogic(words);
+        game1.checkWordMatch(input);
+
+        System.out.println(game1.getModStr());
     }
 }
 
--- WordleLogic.java
+++ WordleLogic.java
@@ -5,16 +5,20 @@ import java.util.HashMap;
 public class WordleLogic
 {
     private final int MAX_TRIES_ALLOWED = 5, MAX_LENGTH = 5;
-    private String[] wordList = new String[20];
+    private final String[] wordList;
     private int cursor;
-    private HashMap<Character, Integer> lettFreqMap = new HashMap<>();
+    private final HashMap<Character, Integer> charFreqMap;
+    private final HashMap<Integer, Character> wordIndexMap;
     private String currWord;
-    private String modVal;
+    private String modStr;
     private boolean gameOver = false;
 
     public WordleLogic(String[] arr) {
         wordList = arr;
         cursor = -1;
+        charFreqMap = new HashMap<>();
+        wordIndexMap = new HashMap<>();
+        modStr = "";
     }
 
     private void setWord() {
@@ -24,16 +28,54 @@ public class WordleLogic
         for (int i = 0; i < MAX_LENGTH; i++)
         {
             int amount = 1;
-            if (lettFreqMap.containsKey(currWord.charAt(i)))
+            if (charFreqMap.containsKey(currWord.charAt(i)))
             {
-                lettFreqMap.put(currWord.charAt(i), amount++);
+                charFreqMap.put(currWord.charAt(i), amount++);
             } else {
-                lettFreqMap.put(currWord.charAt(i), amount);
+                charFreqMap.put(currWord.charAt(i), amount);
             }
+            wordIndexMap.put(i, currWord.charAt(i));
         }
     }
 
     private boolean isWordAcceptable(String userInput) {
         return true; // placeholder
+    }
+
+    public void checkWordMatch(String inputStr)
+    {
+        setWord();
+        assert isWordAcceptable(inputStr);
+
+        char currChar;
+        for (int i = 0; i < inputStr.length(); i++)
+        {
+            currChar = inputStr.charAt(i);
+
+            // character is in the frequency map and its value is > 0
+            if (charFreqMap.containsKey(currChar) && charFreqMap.get(currChar) > 0)
+            {
+                charFreqMap.put(currChar, charFreqMap.get(currChar) - 1); // decrement the frequency of the character
+
+                // checks whether currChar matches the actual character at index i
+                if (currChar == wordIndexMap.get(i))
+                {
+                    modStr += "'" + currChar + "'";
+                }
+                else
+                {
+                    modStr += "*"+ currChar + "*";
+                }
+            }
+            else 
+            {
+                modStr += currChar;
+            }
+        }
+    }
+
+    public String getModStr()
+    {
+        return modStr;
     }
 }
